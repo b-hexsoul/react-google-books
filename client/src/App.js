97bf376d8f4b71ec82e16be0f794089cfe913baa
Brandon Hexsel <66162988+b-hexsoul@users.noreply.github.com>
2021-02-05 04:49:11 -0800

    'Header component'

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,21 @@
-import "./App.css";
+import "./App.scss";
 import { Switch, Route } from "react-router-dom";
 import Nav from "./components/nav/nav.component";
 import Search from "./pages/search/search.page";
 import Saved from "./pages/saved/saved.page";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div>
       <Nav />
-      <Switch>
-        <Route exact path="/" component={Search} />
-        <Route exact path="/saved" component={Saved} />
-      </Switch>
+      <div className="wrapper">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Search} />
+          <Route exact path="/saved" component={Saved} />
+        </Switch>
+      </div>
     </div>
   );
 }
---
+++ client/src/components/header/header.component.jsx
@@ -0,0 +1,12 @@
+import "./header.styles.scss";
+
+const Header = () => {
+  return (
+    <div className="header-container">
+      <h1 className="header-main-text">React Book Search</h1>
+      <p className="header-sub-text">Search for and Save Books of Interest</p>
+    </div>
+  );
+};
+
+export default Header;
